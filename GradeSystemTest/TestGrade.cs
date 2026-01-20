0008efe3b2caec6bb44c22ee5d0b3fde53a90b38
Chaity Sarker <chaity.sarker@symphonysoftt.com>
2026-01-20 10:09:03 +0600

    Fix grade calculation bug

--- GradeSystemTest/TestGrade.cs
+++ GradeSystemTest/TestGrade.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GradeSystem;
+using Xunit;
 
 namespace GradeSystemTest
 {
-    internal class TestGrade
+    public class StudentGradeTests
     {
+        private readonly Grade service = new Grade();
+
+        [Fact]
+        public void Marks_85_Returns_A()
+        {
+            var result = service.GetGrade(85);
+            Assert.Equal("A", result);
+        }
+
+        [Fact]
+        public void Marks_65_Returns_B()
+        {
+            var result = service.GetGrade(65);
+            Assert.Equal("B", result);
+        }
+
+        [Fact]
+        public void Marks_45_Returns_C()
+        {
+            var result = service.GetGrade(45);
+            Assert.Equal("C", result);
+        }
+
+        [Fact]
+        public void Marks_30_Returns_F()
+        {
+            var result = service.GetGrade(30);
+            Assert.Equal("F", result);
+        }
     }
 }
