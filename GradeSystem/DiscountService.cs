a2b55dc339981138701af2e35c1e03adf9be7bd5
Chaity Sarker <chaity.sarker@symphonysoftt.com>
2026-01-22 12:07:59 +0600

    trigger sonar workflow

--- GradeSystem/DiscountService.cs
+++ GradeSystem/DiscountService.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace GradeSystem
 {
-    internal class DiscountService
+    public class DiscountService
     {
+        public double GetDiscount(double amount)
+        {
+            if (amount >= 10000) return amount * 0.20;
+            if (amount >= 5000) return amount * 0.10;
+            if (amount >= 1000) return amount * 0.05;
+            return 0;
+        }
     }
 }
